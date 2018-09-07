4cbf372a62af494cd83983645cf4811aa9b6b917
Mauricio Sodre <mss.junior@globo.com>
2018-09-07 14:13:06 -0300

    EndPoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/mauriciosodre/cursomc/services/PedidoService.java
@@ -0,0 +1,26 @@
+package com.mauriciosodre.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mauriciosodre.cursomc.domain.Pedido;
+import com.mauriciosodre.cursomc.repositories.PedidoRepository;
+import com.mauriciosodre.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+
+		Optional<Pedido> obj = repo.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
