f6712626659811a5b18d76e3dba8806fd0efc4a9
Mauricio Sodre <mss.junior@globo.com>
2018-09-18 17:44:30 -0300

    PUT, DELETE E GET para Cliente

---
+++ src/main/java/com/mauriciosodre/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,51 @@
+package com.mauriciosodre.cursomc.dto;
+
+import java.io.Serializable;
+
+import com.mauriciosodre.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String nome;
+	private String email;
+
+	// Constructors
+	public ClienteDTO() {
+		super();
+	}
+
+	public ClienteDTO(Cliente obj) {
+		super();
+		id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
+	}
+
+	// Getters & Setters
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
