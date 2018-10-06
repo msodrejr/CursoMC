091ae9fc3d58f4f326fa9df7a8aadc92ac2bfa73
Mauricio Sodre <mss.junior@globo.com>
2018-10-06 03:36:42 -0300

    Implementando toString de Pedido

--- src/main/java/com/mauriciosodre/cursomc/domain/ItemPedido.java
+++ src/main/java/com/mauriciosodre/cursomc/domain/ItemPedido.java
@@ -1,6 +1,8 @@
 package com.mauriciosodre.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
@@ -40,7 +42,7 @@ public class ItemPedido implements Serializable {
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-	
+
 	public void setPedido(Pedido pedido) {
 		id.setPedido(pedido);
 	}
@@ -48,7 +50,7 @@ public class ItemPedido implements Serializable {
 	public Produto getProduto() {
 		return id.getProduto();
 	}
-	
+
 	public void setProduto(Produto produto) {
 		id.setProduto(produto);
 	}
@@ -108,6 +110,22 @@ public class ItemPedido implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		StringBuilder builder = new StringBuilder();
+		builder.append(getProduto().getNome());
+		builder.append(", Qte: ");
+		builder.append(getQuantidade());
+		builder.append(", Preço unitário: ");
+		builder.append(nf.format(getPreco()));
+		builder.append(", Subtoltal: ");
+		builder.append(nf.format(getSubTotal()));
+		builder.append("\n");
+
+		return builder.toString();
 	}
 
 }
