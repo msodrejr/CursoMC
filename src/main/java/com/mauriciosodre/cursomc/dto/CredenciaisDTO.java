3bc04990c68d90b26e575cf3658de6ac0dc5db2e
Mauricio Sodre <mss.junior@globo.com>
2018-10-18 06:14:24 -0300

    Implementando autenticacao e geracao do token JWT

---
+++ src/main/java/com/mauriciosodre/cursomc/dto/CredenciaisDTO.java
@@ -0,0 +1,32 @@
+package com.mauriciosodre.cursomc.dto;
+
+import java.io.Serializable;
+
+public class CredenciaisDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String email;
+	private String senha;
+	
+	//Constructors
+	public CredenciaisDTO() {
+		super();
+	}
+	
+	//Getters & Setters
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/mauriciosodre/cursomc/services/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.mauriciosodre.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.mauriciosodre.cursomc.domain.Cliente;
+import com.mauriciosodre.cursomc.repositories.ClienteRepository;
+import com.mauriciosodre.cursomc.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+	
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		
+		Cliente cli = repo.findByEmail(email);
+		
+		if(cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
