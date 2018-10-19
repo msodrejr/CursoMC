9c387e780894f22bf0da8b068f1e6c9e777e4ac9
Mauricio Sodre <mss.junior@globo.com>
2018-10-19 04:17:08 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/com/mauriciosodre/cursomc/config/SecurityConfig.java
+++ src/main/java/com/mauriciosodre/cursomc/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,66 +23,61 @@ import com.mauriciosodre.cursomc.security.JWTAuthenticationFilter;
 import com.mauriciosodre.cursomc.security.JWTAuthorizationFilter;
 import com.mauriciosodre.cursomc.security.JWTUtil;
 
-
-
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 	@Autowired
 	private Environment env;
 	@Autowired
 	private UserDetailsService userDetailsService;
 	@Autowired
 	private JWTUtil jwtUtil;
+
+	public static final String[] PUBLIC_MATCHERS = { "/h2-console/**", };
+
+	public static final String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**"};
 	
-	public static final String[] PUBLIC_MATCHERS = {
-		"/h2-console/**",
-	};
-	
-	public static final String[] PUBLIC_MATCHERS_GET = {
-			"/produtos/**",
-			"/categorias/**",
-			"/clientes/**"
-		};
-	
+	public static final String[] PUBLIC_MATCHERS_POST = { "/clientes/**"};
+
 	@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		
-		//Verifica se o profile ativo é de teste, para liberar o h2-console
-		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+	protected void configure(HttpSecurity http) throws Exception {
+
+		// Verifica se o profile ativo é de teste, para liberar o h2-console
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
-		
+
 		http.cors().and().csrf().disable();
-		http.authorizeRequests()
-		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
-		.antMatchers(PUBLIC_MATCHERS).permitAll()
-		.anyRequest().authenticated();
-		
-		//Filtro de autenticação
+		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+				.antMatchers(PUBLIC_MATCHERS).permitAll()
+				.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+				.anyRequest().authenticated();
+
+		// Filtro de autenticação
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-		//Filtro de autorização
+		// Filtro de autorização
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
-		
+
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
-	
-protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-		
+
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 
 	@Bean
-	  CorsConfigurationSource corsConfigurationSource() {
-	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-	    return source;
-	  }
-	
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 }
